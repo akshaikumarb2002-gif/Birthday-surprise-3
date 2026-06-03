dd2735a8ea8df5b6df2499262f6d94e986a246ca
akshaikumarb2002-gif <akshaikumarb2002@gmail.com>
2026-06-04 03:58:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+const openBtn = document.getElementById("openBtn");
+
+openBtn.addEventListener("click", () => {
+
+document.getElementById("welcome-screen").style.display = "none";
+
+document.getElementById("birthday-screen").style.display = "block";
+
+});
+
+function reveal(card){
+
+let img = card.querySelector("img");
+let text = card.querySelector("span");
+let tap = card.querySelector("p");
+
+img.style.display="block";
+text.style.display="block";
+tap.style.display="none";
+
+}
